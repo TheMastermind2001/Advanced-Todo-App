198b88ff7faac03d0139905f25a1bd7b94f9dc95
Agniva Bhattacharya <agnivabhattacharya2001@gmail.com>
2024-02-02 03:13:40 +0530

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express=require("express");
+const app=express();
+app.use(express.json());
+
+app.post("/todo",(req,res)=>{
+
+})
+
+app.get("/todo",(req,res)=>{
+    
+})
+
+app.put("/completed",(req,res)=>{
+    
+})
